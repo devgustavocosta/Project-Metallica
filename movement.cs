d2adfefb47e38b90348896c4b2a1cd63b232b2fe
Gustavo Costa da Silva <gustavoc@dbserver.com.br>
2022-09-27 21:06:13 -0300

    second commit

--- movement.cs
+++ movement.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class movement2D : MonoBehaviour
+ 
+public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D body;
-
-    float horizontal;
-    float vertical;
-
-    public float runSpeed = 10.0f;
-    
-
+    public Animator playerAnimator;
+    float input_x = 0;
+    float input_y = 0;
+    public float speed = 2.5f;
+    bool isWalking = false;
+ 
+    // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();        
+        isWalking = false;   
     }
-
+ 
+    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-    }
-
-    private void FixedUpdate()
-    {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);   
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_y = Input.GetAxisRaw("Vertical");
+        isWalking = (input_x != 0 || input_y != 0);
+ 
+        if (isWalking)
+        {
+            var move = new Vector3(input_x, input_y, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            playerAnimator.SetFloat("input_x", input_x);
+            playerAnimator.SetFloat("input_y", input_y);
+        }
+ 
+        playerAnimator.SetBool("isWalking", isWalking);
+ 
+        if (Input.GetButtonDown("Fire1"))
+            playerAnimator.SetTrigger("attack");
     }
 }
